e189f79436c68a68a2d499c94b07267481d94db5
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2022-02-18 19:09:08 +0200

    create association between books and authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class Author < ApplicationRecord
-  has_many :books, dependent: :delete_all
+  has_many :authors_books, dependent: :nullify
+  has_many :books, through: :authors_books
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@
 class Book < ApplicationRecord
   include ImageUploader::Attachment(:image)
   belongs_to :category
+  has_many :authors_books, dependent: :destroy
+  has_many :authors, through: :authors_books
 end
