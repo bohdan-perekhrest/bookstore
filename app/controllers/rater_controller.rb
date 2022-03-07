854fa073dbe134253278ca7b7e94079775272eed
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2022-03-07 15:02:13 +0200

    remove rater_controller

--- app/controllers/rater_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RaterController < ApplicationController
-  def create
-    obj = params[:klass].classify.constantize.find(params[:id])
-    obj.rate params[:score].to_f, current_user
-  end
-end
