0dbdc713ea948ec5b930ead26b341f5ae59bc31c
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2022-03-20 18:31:26 +0200

    update step3

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,9 +24,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'bohdan.perekhrest@rubygarage.org'
+  config.mailer_sender = 'noreply@bookstore.com'
   # Configure the class responsible to send e-mails.
-  config.mailer = 'Devise::Mailer'
+  # config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
