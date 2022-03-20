736ff231f1991a435deaf638cf0b0f66e1c6aded
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2022-03-20 18:15:48 +0200

    update step3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          # :omiauthable
-  validates :email, format: { with: /\A[^-.]\w+[-.]?(\w+[-!#$%&'*+\/=?^_`{|}~.]\w+)*[^-]@([\w\d]+)\.([\w\d]+)\z/ }
+
   validates :password, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}\z/  }
 end
