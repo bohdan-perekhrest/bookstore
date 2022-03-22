2714ad23eba8170fed24ea6026e21336c0590410
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2022-03-22 13:43:09 +0200

    update step7

--- app/presenters/order_presenter.rb
+++ app/presenters/order_presenter.rb
@@ -2,6 +2,14 @@
 
 class OrderPresenter < BasePresenter
   attr_reader :order
+  
+  ORDER_STATUSES = {
+    in_progress: I18n.t('order.status.in_progress'),
+    in_queue: I18n.t('order.status.in_queue'),
+    in_delivery: I18n.t('order.status.in_delivery'),
+    delivered: I18n.t('order.status.delivered'),
+    canceled: I18n.t('order.status.canceled')
+  }.freeze
 
   def initialize(order)
     @order = order
