f97f35b313376b5e5fd2c9505edb35a8c0c8f31b
Bogdan Perekhrest <bogdan.perekhrest34@gmail.com>
2022-03-31 14:50:22 +0300

    update specs

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Coupon, type: :model do
   it { expect(subject).to validate_presence_of :code }
   it { expect(subject).to validate_presence_of :value }
   it { expect(subject).to validate_length_of(:code).is_equal_to 15 }
-  it { expect(subject).to validate_uniqueness_of :code }
   it { expect(subject).to validate_numericality_of(:value) }
   it { expect(subject).to have_many :orders }
 end
--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe CreditCard, type: :model do
-  context 'validations' do
-    it { expect(subject).to validate_presence_of :number }
-    it { expect(subject).to validate_presence_of :name }
-    it { expect(subject).to validate_presence_of :mm_yy }
-    it { expect(subject).to have_one :order }
-    it { expect(subject).to allow_value('09/22').for(:mm_yy) }
-    it { expect(subject).to allow_value('12/23').for(:mm_yy) }
-    it { expect(subject).to allow_value('02/24').for(:mm_yy) }
-    it { expect(subject).not_to allow_value('00/17').for(:mm_yy) }
-    it { expect(subject).not_to allow_value('21/17').for(:mm_yy) }
-    it { expect(subject).not_to allow_value('1f/ac').for(:mm_yy) }
-    it { expect(subject).to allow_value('1234567891234571').for(:number) }
-    it { expect(subject).not_to allow_value('123456').for(:number) }
-    it { expect(subject).to allow_value('Adam').for(:name) }
-    it { expect(subject).to allow_value('Eva').for(:name) }
-    it { expect(subject).not_to allow_value('').for(:name) }
-    it { expect(subject).not_to allow_value('!@#$%^&*_+').for(:name) }
-  end
+  it { expect(subject).to validate_presence_of :number }
+  it { expect(subject).to validate_presence_of :name }
+  it { expect(subject).to validate_presence_of :mm_yy }
+  it { expect(subject).to have_one :order }
+  it { expect(subject).to allow_value('09/22').for(:mm_yy) }
+  it { expect(subject).to allow_value('12/23').for(:mm_yy) }
+  it { expect(subject).to allow_value('02/24').for(:mm_yy) }
+  it { expect(subject).not_to allow_value('00/17').for(:mm_yy) }
+  it { expect(subject).not_to allow_value('21/17').for(:mm_yy) }
+  it { expect(subject).not_to allow_value('1f/ac').for(:mm_yy) }
+  it { expect(subject).to allow_value('1234567891234571').for(:number) }
+  it { expect(subject).not_to allow_value('123456').for(:number) }
+  it { expect(subject).to allow_value('Adam').for(:name) }
+  it { expect(subject).to allow_value('Eva').for(:name) }
+  it { expect(subject).not_to allow_value('').for(:name) }
+  it { expect(subject).not_to allow_value('!@#$%^&*_+').for(:name) }
 end
